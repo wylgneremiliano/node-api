7d779c631ec8f1e73e503ba4212bf5b768ae3fa4
Wylgner Emiliano <wylgneremiliano@gmail.com>
2019-09-25 23:22:34 -0300

    +cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 // Iniciando o App
 const app = express();
 app.use(express.json());
+app.use(cors());
 // Iniciando o banco de dados
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
