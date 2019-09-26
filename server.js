8cc773b74035bf868063b268253781967da1615e
Wylgner Emiliano <wylgneremiliano@gmail.com>
2019-09-25 21:09:42 -0300

    Arquivos estruturados + crud produtos iniciado

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+// Iniciando o App
+const app = express();
+
+// Iniciando o banco de dados
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+requireDir("./src/models/");
+
+
+
+// Rotas
+app.use("/api", require('./src/routes'));
+app.listen(3001);
