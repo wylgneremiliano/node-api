21e3229d489fc80a59fbbadaa41509b8923dac3b
Wylgner Emiliano <wylgneremiliano@gmail.com>
2019-09-25 23:01:53 -0300

    crud completo + paginação

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 // Iniciando o App
 const app = express();
-
+app.use(express.json());
 // Iniciando o banco de dados
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
@@ -12,8 +12,6 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
 
 requireDir("./src/models/");
 
-
-
 // Rotas
-app.use("/api", require('./src/routes'));
+app.use("/api", require("./src/routes"));
 app.listen(3001);
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const mongoosePaginate = require("mongoose-paginate");
 const ProductSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -19,4 +19,5 @@ const ProductSchema = new mongoose.Schema({
   }
 });
 
+ProductSchema.plugin(mongoosePaginate);
 mongoose.model("Product", ProductSchema);
